7a4b476990a8f722d37a916c27453fe2977374a9
owen <owenhuang91@gmail.com>
2016-08-01 10:04:35 +0800

    commit

---
+++ HelloWorld/models/mysql.js
@@ -0,0 +1,16 @@
+﻿var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'owen286236',
+    port: 3306
+});
+
+connection.connect();
+
+connection.query('SELECT 12 + 34 AS result', function (err, rows, fields) {
+    if (err) throw err;
+    console.log('The result is: ', rows[0].result);
+}); 
+
+connection.end();
---
+++ HelloWorld/routes/index.js
@@ -0,0 +1,40 @@
+﻿var mySQL = require('mysql');
+var DBOption = {
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'charge',
+    port: 3306
+};
+var connection = mySQL.createConnection(DBOption);
+/*
+ * GET home page.
+ */
+
+exports.index = function (req, res) {
+    res.render('index');
+};
+
+exports.ChargeType = function (req, res) {
+    connection.query("SELECT * FROM CHARGE_TYPE ORDER BY MAIN_TYPE,SUB_TYPE", function (err, rows, fields) {
+        if (err) throw err;
+        else res.render('ChargeType', { ChargeTypeData: rows });
+    });
+    
+
+};
+
+exports.ChargeTypeCreate = function (req, res) {
+    var insertSQL = [' INSERT INTO CHARGE_TYPE (MAIN_TYPE, SUB_TYPE,DESCRIPTION,LAST_UPDATED_TIMESTAMP) ',
+        ' VALUES(?,?,?,NOW()) '].join('');
+    
+    connection.query(insertSQL, [req.body.MainType, req.body.SubType, req.body.Description] , function (err, rows, fields) {
+        if (err) throw err;
+        else res.send({ updateflag: true, returnMessage: "Create successful." });
+    });
+};
+
+exports.contact = function (req, res) {
+    res.render('contact', { title: 'Contact', year: new Date().getFullYear(), message: 'Your contact page' });
+};
+
